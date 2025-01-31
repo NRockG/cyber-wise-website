cc72b28f406be15ca8943b763657b87d0b672dcd
Naebi Sede <willford.fx@gmail.com>
2025-01-31 12:52:57 +0100

    Modified scripts.js

--- scripts.js
+++ scripts.js
@@ -1,7 +1,7 @@
 async function chatWithCyberdoc() {
     let userMessage = document.getElementById("userInput").value;
 
-    let response = await fetch("https://gravity30-cyberwise.hf.space", {
+    let response = await fetch("https://gravity30-cyberwise.hf.space/chat", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ message: userMessage })
